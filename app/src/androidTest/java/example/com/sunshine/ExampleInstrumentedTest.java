bcaf8f0d9e15d84f850f92f8c880330076366643
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2018-01-11 17:28:25 +0800

    Implemented simple test

--- app/src/androidTest/java/example/com/sunshine/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/example/com/sunshine/ExampleInstrumentedTest.java
@@ -1,26 +1,24 @@
 package example.com.sunshine;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.os.Build;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("example.com.sunshine", appContext.getPackageName());
+    /**
+     * Simple test assuring that the current version of Android is greater than Gingerbread
+     */
+    @Test
+    public void testAndroidVersionGreaterThanGingerbread() {
+        int currentVersion = Build.VERSION.SDK_INT;
+        int gingerbread = Build.VERSION_CODES.GINGERBREAD;
+
+        assertTrue(currentVersion > gingerbread);
     }
 }
