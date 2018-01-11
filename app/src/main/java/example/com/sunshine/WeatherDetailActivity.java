905b229eeeb4d155a749a1733ec4b92b0cc9d9fa
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2018-01-11 18:13:26 +0800

    S04.03-Solution-AddMapAndSharing

--- app/src/main/java/example/com/sunshine/WeatherDetailActivity.java
+++ app/src/main/java/example/com/sunshine/WeatherDetailActivity.java
@@ -1,8 +1,10 @@
 package example.com.sunshine;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.widget.TextView;
 
 public class WeatherDetailActivity extends AppCompatActivity {
@@ -19,5 +21,30 @@ public class WeatherDetailActivity extends AppCompatActivity {
         if (getIntent().hasExtra(Intent.EXTRA_TEXT)) {
             mTvWeatherDetail.setText(getIntent().getStringExtra(Intent.EXTRA_TEXT));
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.weather_detail_menu, menu);
+
+        menu.findItem(R.id.menu_action_share)
+            .setIntent(createShareForecastIntent());
+
+        return true;
+    }
+
+    /**
+     * Uses the ShareCompat Intent builder to create our Forecast intent for sharing. We set the
+     * type of content that we are sharing (just regular text), the text itself, and we return the
+     * newly created Intent.
+     *
+     * @return The Intent to use to start our share.
+     */
+    private Intent createShareForecastIntent() {
+        return ShareCompat.IntentBuilder
+            .from(this)
+            .setType("text/plain")
+            .setText(getIntent().getStringExtra(Intent.EXTRA_TEXT))
+            .getIntent();
     }
 }
